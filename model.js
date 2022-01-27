246504e04da72112d21ad3028f55ee303acb52da
attoumbre <juvenalak@gmail.com>
2022-01-27 19:53:34 +0100

    implementation des classe du modele

--- model.js
+++ model.js
@@ -1,3 +1,48 @@
 
 // Implémenter ici les 4 classes du modèle.
+
+function Drawing(){
+
+    this.myforms = new Array();
+    this.getMyforms = function(){
+        return this.myforms;
+    }.bind(this);
+
+    this.addForm = function(form){
+        this.myforms.push(form);
+    }.bind(this);
+}
+
+function Form(posiInitX,posiInitY,posiFinX,posiFinY, epaisseur,couleur){
+    this.posiInitX = posiInitX;
+    this.posiInitY = posiInitY;
+    this.posiFinX = posiFinX;
+    this.posiInitY = posiFinY;
+    this.epaisseur = epaisseur;
+    this.couleur = couleur;
+
+    this.getPosiInitX = function(){
+        return this.posiInitX;
+    }.bind(this);
+
+    this.getPosiInitX = function(){
+        return this.posiInitY;
+    }.bind(this);
+
+    this.getPosiFinX = function(){
+        return this.posiFinX;
+    }.bind(this);
+
+    this.getPosiFinY = function(){
+        return this.posiFinY;
+    }.bind(this);
+}
+
+function Rectangle(){
+    Form.call(posiInitX,posiInitY,posiFinX,posiFinY, epaisseur,couleur)
+}
+
+function Line(){
+    Form.call(posiInitX,posiInitY,posiFinX,posiFinY, epaisseur,couleur)
+}
 // N'oubliez pas l'héritage !
