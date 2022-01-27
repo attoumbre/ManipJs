9ca5e5dcf6032f8dbb6d62051c08e1e0c6f011b5
attoumbre <juvenalak@gmail.com>
2022-01-27 18:25:09 +0100

    creation des methodes de gestion d'evenements

--- interaction.js
+++ interaction.js
@@ -7,9 +7,51 @@ function DnD(canvas, interactor) {
   this.posiInitY = 0;
   this.posiFinX = 0;
   this.posiFinY = 0;
+  this.isPress = false;
+  
+ 
+
 	// Developper les 3 fonctions gérant les événements
 
+  DnD.prototype.maFctGerantLaPression = function(evt){
+
+    this.isPress = true;
+    this.posiInitX = getMousePosition(canvas, evt).x;
+    this.posiInitY = getMousePosition(canvas, evt).y;
+
+    console.log(this.posiInitX);
+    console.log(this.posiInitY);
+
+  
+  }.bind(this);
+  
+  DnD.prototype.maFctGerantLeDeplacement = function(evt){
+  
+    if(this.isPress){
+      this.posiFinX = getMousePosition(canvas, evt).x;
+      this.posiFinY = getMousePosition(canvas, evt).y;
+      console.log(this.posiFinX);
+      console.log(this.posiFinY);
+    }
+    
+
+  
+  }.bind(this);
+  
+  DnD.prototype.maFctGerantLeRelachement = function(evt){
+    this.isPress = false;
+    var res = getMousePosition(canvas, evt);
+    this.posiFinX = res.x;
+    this.posiFinY = res.y;
+    console.log(this.posiFinX);
+    console.log(this.posiFinY);
+
+  }.bind(this);
 	// Associer les fonctions précédentes aux évènements du canvas.
+
+  canvas.addEventListener('mousedown', this.maFctGerantLaPression, false);
+  canvas.addEventListener('mousemove', this.maFctGerantLeDeplacement, false);
+  canvas.addEventListener('mouseup', this.maFctGerantLeRelachement, false);
 };
 
 
@@ -21,6 +63,3 @@ function getMousePosition(canvas, evt) {
     y: evt.clientY - rect.top
   };
 };
-
-
-
