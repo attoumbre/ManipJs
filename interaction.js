a424762471fc40f3e751fe7ccf5ae6c022686ab3
attoumbre <juvenalak@gmail.com>
2022-01-27 15:28:20 +0100

    first commit

---
+++ interaction.js
@@ -0,0 +1,26 @@
+
+// La création d'un Dnd requière un canvas et un interacteur.
+// L'interacteur viendra dans un second temps donc ne vous en souciez pas au départ.
+function DnD(canvas, interactor) {
+	// Définir ici les attributs de la 'classe'
+  this.posiInitX = 0;
+  this.posiInitY = 0;
+  this.posiFinX = 0;
+  this.posiFinY = 0;
+	// Developper les 3 fonctions gérant les événements
+
+	// Associer les fonctions précédentes aux évènements du canvas.
+};
+
+
+// Place le point de l'événement evt relativement à la position du canvas.
+function getMousePosition(canvas, evt) {
+  var rect = canvas.getBoundingClientRect();
+  return {
+    x: evt.clientX - rect.left,
+    y: evt.clientY - rect.top
+  };
+};
+
+
+
